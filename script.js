5d4e752c1aa9269c02e8ea7ab6170b6d68ca96de
dharshanaks005 <dharshanaks005@gmail.com>
2024-09-27 21:42:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var button = document.getElementById("button1");
+var footer= document.getElementById("footer");
+button.addEventListener("click", clicked);
+
+function clicked(){
+    var name = prompt("Your name");
+    var footer= document.getElementById("footer");
+    footer.innerText=name;
+}
+    
+var x =["value1","value2","value3","value4"];
+
+
+//how it gets added up which is undecided like insta reels//
+
